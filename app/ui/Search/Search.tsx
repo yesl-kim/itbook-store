49454b80df10b52c5e3805d17408e248671be0ca
yesl-kim <yesl00k624@gmail.com>
2023-11-24 16:25:41 +0900

    feat: 검색 키워드 유효성검사

--- app/ui/Search/Search.tsx
+++ app/ui/Search/Search.tsx
@@ -2,6 +2,7 @@
 
 import { useSearchParams } from 'next/navigation'
 import { useRouter } from 'next/navigation'
+import { useCallback } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 interface SearchForm {
@@ -12,7 +13,11 @@ const PATH = '/books'
 export default function Search() {
   const searchParams = useSearchParams()
   const { push } = useRouter()
-  const { register, handleSubmit } = useForm<SearchForm>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SearchForm>({
     defaultValues: {
       keyword: searchParams.get('query')?.toString(),
     },
@@ -23,20 +28,41 @@ export default function Search() {
     params.set('query', keyword)
     params.set('page', '1')
     push(`${PATH}?${params.toString()}`)
+    const keywords = parseKeyword(keyword)
+    // TODO: keywords를 여러개 전달해야함 ex) ?q=a&q=b...
   }
 
   return (
-    <form
-      className="relative flex flex-1 flex-shrink-0"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <input
-        {...register('keyword', { required: true })}
-        role="search"
-        placeholder="검색어를 입력해주세요"
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 bg-black"
-      />
-      <input type="submit" value="검색" />
-    </form>
+    <div>
+      <form
+        className="relative flex flex-1 flex-shrink-0"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          {...register('keyword', {
+            required: true,
+            validate: validateMaxKeyword,
+          })}
+          role="search"
+          placeholder="검색어를 입력해주세요"
+          className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        />
+        <input type="submit" value="검색" />
+      </form>
+      <p>{errors.keyword?.message}</p>
+    </div>
   )
+}
+
+const parseKeyword = (keyword: string): string[] => {
+  const seperatorRegex = /[\|\-]/
+  return keyword.split(seperatorRegex)
+}
+
+const validateMaxKeyword = (input: string) => {
+  const keywords = parseKeyword(input)
+  if (keywords.length > 2) {
+    return '키워드는 최대 2개까지 입력할 수 있습니다'
+  }
+  return true
 }
