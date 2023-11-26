f2d0217174be99c84ee12c4b56a1f1139efc72af
yesl-kim <yesl00k624@gmail.com>
2023-11-26 15:05:01 +0900

    refactor: fetchMore 함수 분리

--- app/books/components/load-more.tsx
+++ app/books/components/load-more.tsx
@@ -1,63 +1,20 @@
 'use client'
 
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
 import BookList from './book-list'
-import { useSearchParams } from 'next/navigation'
-import { parseKeyword } from '@/app/lib/parser'
-import { searchBooks } from '@/app/actions/search-books'
+import useSearchBooks from '../lib/useSearchBooks'
 
 const LoadMore = () => {
+  const { loading, data: books, fetchMore } = useSearchBooks()
+
   const { ref, inView } = useInView()
-  const [loadedPage, setPage] = useState(1)
-  const [books, setBooks] = useState<BooksItem[]>([])
-  const params = useSearchParams()
-  const [loading, setLoading] = useState(false)
-  const [done, setDone] = useState(false)
-
-  const fetchMore = useCallback(async () => {
-    if (done) {
-      return
-    }
-
-    try {
-      setLoading(true)
-
-      const nextPage = loadedPage + 1
-      const query = params.get('query')
-      // FIXME 🤔
-      if (!query) {
-        return
-      }
-
-      const { includeKeywords, nonIncludeKeywords } = parseKeyword(query)
-      const books = await searchBooks(
-        includeKeywords,
-        nonIncludeKeywords,
-        nextPage
-      )
-
-      if (books.length === 0) {
-        setDone(true)
-        return
-      }
-
-      setBooks((prev) => prev.concat(books))
-      setPage(nextPage)
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setLoading(false)
-    }
-  }, [loadedPage, params, done])
-
   useEffect(() => {
-    console.log(inView)
     if (inView) {
-      console.log('fetch more!')
       fetchMore()
     }
   }, [inView, fetchMore])
+
   return (
     <>
       <BookList list={books} />
