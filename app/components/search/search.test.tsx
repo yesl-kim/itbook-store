4a2f60c2c30f8b79cbbcdc84fbe08822259db52f
yesl-kim <yesl00k624@gmail.com>
2023-11-29 23:35:33 +0900

    test: Search component

--- app/components/search/search.test.tsx
+++ app/components/search/search.test.tsx
@@ -1,28 +1,70 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import mockRouter from 'next-router-mock'
 
 import { routes } from 'constants/routes'
 import Search from './search'
+import { validateMaxKeywords } from './validate'
 
-jest.mock('next/navigation', () => jest.requireActual('next-router-mock'))
+jest.mock('next/navigation', () => {
+  return {
+    __esModule: true,
+    useRouter: () => mockRouter,
+    useSearchParams: () => new URLSearchParams(),
+  }
+})
+
+interface InitialValue {
+  input?: string
+  route?: string
+}
+
+const search = async (initialValue = {} as InitialValue) => {
+  const { input = '', route = '/' } = initialValue
+  mockRouter.push(route)
+  render(<Search />)
+
+  const search = screen.getByRole('search')
+  const submitButton = screen.getByDisplayValue('검색')
+  fireEvent.change(search, { target: { value: input } })
+  await waitFor(async () => fireEvent.submit(submitButton))
+
+  return { router: mockRouter, search }
+}
+
+const initialRoute = {
+  asPath: '/',
+  query: {},
+}
 
 describe('search component', () => {
-  it('검색하면 입력값이 쿼리 파라미터("query")에 담겨 "books/"로 이동한다', () => {
-    mockRouter.push('/')
-    render(<Search />)
+  it('입력값이 주어지지 않은 경우 검색은 무시된다', async () => {
+    const { router } = await search()
+    expect(router).toMatchObject(initialRoute)
+  })
 
+  it('검색하면 입력값이 쿼리 파라미터("query")에 담겨 "books/"로 이동한다', async () => {
     const input = 'keyword'
-    const search = screen.getByRole('search')
-    fireEvent.change(search, { target: { value: input } })
+    const { router } = await search({ input })
 
-    expect(mockRouter).toMatchObject({
+    expect(router).toMatchObject({
       asPath: `${routes.search_books}?query=${input}`,
       pathname: routes.search_books,
       query: { query: input },
     })
   })
-  it('입력값이 주어지지 않은 경우 검색은 무시된다', () => {})
-  it('"|", "-"로 구분되는 검색 키워드는 최대 2개까지만 입력될 수 있다. 넘어갈 경우 검색이 무시된다', () => {})
-  test('snapshot', () => {})
+
+  it('"|", "-"로 구분되는 검색 키워드가 2개 이상 입력될 경우 검색이 무시되고 에러메세지가 출력된다', async () => {
+    const input = 'keyword1|keyword2-not'
+    const { router } = await search({ input })
+    const errorMessage = validateMaxKeywords(input)
+
+    expect(errorMessage).not.toBe(true)
+    expect(await screen.findByText(errorMessage as string)).toBeInTheDocument()
+    expect(router).toMatchObject(initialRoute)
+  })
+
+  test('snapshot', () => {
+    expect(render(<Search />)).toMatchSnapshot()
+  })
 })
--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/app/components/$1',
-  },
+  moduleDirectories: ['node_modules', 'app'],
+  modulePaths: ['<rootDir>/app/'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
